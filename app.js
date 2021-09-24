11dd4cd3e68366acce41704d6eae8edecbda09cc
maxime-rgb <maximethomas404@gmail.com>
2021-09-24 16:52:29 +0200

    third

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
  
-var app = express();
+const app = express();
 
 
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3001");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+  next();
+});
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
 
 app.use(logger('dev'));
 app.use(express.json());
--- routes/index.js
+++ routes/index.js
@@ -1,52 +1,231 @@
-var express = require('express');
-var app = express();
-var router = express.Router();
-var cors = require('cors')
+const express = require('express');
+const app = express();
+const router = express.Router();
+const cors = require('cors')
 const mysql = require('mysql');
+var bodyParser = require('body-parser')
 
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+// configure the app to use bodyParser()
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 
-const corsOption= {
-  origin: 'http://localhost:3001',
-  optionSuccessStatus: 200
-}
-
-
-
-router.get('/', cors(corsOption), function (req, res) {
-
-  const db = mysql.createConnection({
+const db = mysql.createConnection({
 
     host: "127.0.0.1",
     user: "root",
     password: "",
-    database : 'make_your_choice'
-  });
-  
-  db.connect()
-  db.query('SELECT * FROM Users, Surveys, Answers', function (error, results, fields) {
-    if (error) throw error;
-    console.log('The solution is: ', results);
-    res.json(results)
-  });
-
-  db.query("INSERT INTO Surveys (Title, Question) VALUES ('?','?')",
-  con.query(sql, function (err, result) {
-    if (err) throw err;
-    console.log("1 record inserted");
- 
-  });
-  
+    database: 'make_your_choice'
+});
+db.connect()
 
 
 
 
+router.get('/Surveys/:id', function (req, res) {
+
+    const creator_id = parseInt(req.params.id)
+    db.config.queryFormat = function (query, values) {
+        if (!values) return query;
+        return query.replace(/\:(\w+)/g, function (txt, key) {
+            if (values.hasOwnProperty(key)) {
+                return this.escape(values[key]);
+            }
+            return txt;
+        }.bind(this));
+    };
+
+    db.query("SELECT Surveys.id, Surveys.Date, Surveys.Title, Users.Firstname FROM `Surveys` JOIN Users ON Users.id = Surveys.User_id WHERE Surveys.User_id = :id LIMIT 10", {id:creator_id}, (err, result) => {
+        if (err) {
+            res.json({ message: "SQL Error" })
+        }
+        if (result.length == 0) {
+            res.json({ message: "Il n'existe pas de sondage" })
+        } else {
+            console.log(result);
+            res.json(result)
+        }
+    })
+})
+
+router.get('/SurveyDetails/:id', function (req, res) {
+
+    const creator_id = parseInt(req.params.id)
+    db.config.queryFormat = function (query, values) {
+        if (!values) return query;
+        return query.replace(/\:(\w+)/g, function (txt, key) {
+            if (values.hasOwnProperty(key)) {
+                return this.escape(values[key]);
+            }
+            return txt;
+        }.bind(this));
+    };
+
+    db.query("SELECT Surveys.id, Surveys.Date, Surveys.Question, Surveys.Title, Users.Firstname FROM `Surveys` JOIN Users ON Users.id = Surveys.User_id WHERE Surveys.User_id = :id LIMIT 10", {id:creator_id}, (err, result) => {
+        if (err) {
+            res.json({ message: "SQL Error" })
+        }
+        if (result.length == 0) {
+            res.json({ message: "Il n'existe pas de sondage" })
+        } else {
+            console.log(result);
+            res.json(result)
+        }
+    })
+})
+
+
+
+router.post('/register', function (req, res) {
+    const FirstName = req.body.FirstName
+    const LastName = req.body.LastName
+    const Mail = req.body.Mail
+    const Password = req.body.Password
+    db.config.queryFormat = function (query, values) {
+        if (!values) return query;
+        return query.replace(/\:(\w+)/g, function (txt, key) {
+            if (values.hasOwnProperty(key)) {
+                return this.escape(values[key]);
+            }
+            return txt;
+        }.bind(this));
+    };
+
+    function insert() {
+
+        db.query(
+            "INSERT INTO Users (LastName, FirstName, Mail, Password) VALUES (:lastname,:firstname,:mail,:password)",
+            { lastname: LastName, firstname: FirstName, mail: Mail, password: Password },
+            (err, result) => {
+                if (err) {
+                    res.json({ message: 'Erreur lors de l\'enregistrement de l\'utilisateur' })
+                }
+                res.json({
+                    id: result.insertId,
+                    FirstName: req.body.FirstName,
+                    LastName: req.body.LastName,
+                    Mail: req.body.Mail,
+                    Password: req.body.Password,
+                })
+            }
+        );
+    }
+    db.query("SELECT * From Users Where Mail = :mail", { mail: Mail }, (err, result) => {
+        if (err) {
+            res.json({ message: "SQL Error" })
+        }
+        if (result.length == 0) {
+            insert()
+        } else {
+            res.json({ message: "L'utilisateur existe déja" })
+        }
+    })
+});
+
+router.post('/login', function (req, res) {
+    console.log(req.body);
+    const Mail = req.body.Mail
+    const Password = req.body.Password
+    db.config.queryFormat = function (query, values) {
+        if (!values) return query;
+        return query.replace(/\:(\w+)/g, function (txt, key) {
+            if (values.hasOwnProperty(key)) {
+                return this.escape(values[key]);
+            }
+            return txt;
+        }.bind(this));
+    };
+
+
+    db.query("SELECT * From Users Where Mail = :mail AND Password = :password", { mail: Mail, password: Password }, (err, result) => {
+        if (err) {
+            res.json({ message: "SQL Error" })
+        }
+        if (result.length == 0) {
+            res.json({ message: "L'utilisateur n'existe pas" })
+        } else {
+            console.log(result[0].id);
+            res.json({
+                id: result[0].id,
+                FirstName: result[0].FirstName,
+                LastName: result[0].LastName,
+                Mail: result[0].Mail,
+                Password: result[0].Password
+            })
+        }
+    })
+});
+
+
+router.put('/Profil/:id', (req, res) => {
+
+    const id = parseInt(req.params.id)
+    const FirstName = req.body.FirstName
+    const LastName = req.body.LastName
+    const Password = req.body.Password
+
+
+    db.config.queryFormat = function (query, values) {
+        if (!values) return query;
+        return query.replace(/\:(\w+)/g, function (txt, key) {
+            if (values.hasOwnProperty(key)) {
+                return this.escape(values[key]);
+            }
+            return txt;
+        }.bind(this));
+    };
+    selectUser(id, false)
+
+    function update() {
+
+
+        db.query(
+            "UPDATE Users SET LastName = :lastname, FirstName = :firstname, Password =:password WHERE id = :id",
+            { id: id, lastname: LastName, firstname: FirstName, password: Password },
+            (err, result) => {
+                if (err) {
+                    res.json({ message: 'Erreur lors de l\'enregistrement de l\'utilisateur' });
+                    return;
+                }
+                selectUser(id, true)
+            }
+        );
+    }
+
+    function selectUser(id, response) {
+
+        db.query("SELECT * From Users Where id = :id", { id: id }, (err, result) => {
+            if (err) {
+                res.json({ message: "SQL Error" })
+            }
+            if (result.length == 0) {
+                res.json({ message: "L'utilisateur n'éxiste pas" })
+            } else {
+                if (response) {
+                    res.json({
+                        id: result[0].id,
+                        FirstName: result[0].FirstName,
+                        LastName: result[0].LastName,
+                        Password: result[0].Password,
+                        Mail: result[0].Mail,
+                    })
+                } else {
+                    update()
+                }
+            }
+        })
+    }
 
 })
 
 
 
 module.exports = router;
+
 
 
